fcebad2ecc991a932b47cab212d34aa6ddc9dd05
Donnie Grubat <donniegrubat2005@gmail.com>
2018-07-08 12:16:14 +0800

    commit middleware

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Middleware\preventBackButton;
 
 Route::get('/', function () {
     //return view('welcome');
@@ -18,7 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::group(['middleware'  => ['preventbackbutton','auth']], function () {
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::resource('employees','EmployeesController');
 
+});
